d5709967478c9536bea6a2851b56955da9c5a76c
疯狂的陈俊 <85547436+juehackr@users.noreply.github.com>
2024-06-17 10:05:33 +0800

    再次优化翻译逻辑~

--- __init__.py
+++ __init__.py
@@ -1,11 +1,10 @@
-print("\nfkserver:OK!\n")
-
+print("===============FkServer:OK!===============")
 import sys, os
 sys.path.insert(0,os.path.dirname(os.path.realpath(__file__)))
 from .fk_server import  Cancelled
 
 NODE_CLASS_MAPPINGS = {}
 WEB_DIRECTORY = "web"
-
 __all__ = ["NODE_CLASS_MAPPINGS"]
-IP_VERSION = "1.0"
+
+VERSION = "1.0"
