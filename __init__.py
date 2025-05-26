bc0437e27c1a8ddc2de0deaa0c17eb04a347f8c9
juehackr <admin@fkdmg.com>
2025-05-26 16:05:21 +0800

    新增更易用的3D姿态编辑器(3D Openpose Editor)节点

--- __init__.py
+++ __init__.py
@@ -2,19 +2,20 @@ import sys, os
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 from .fk_server import Cancelled
 from .server import Cancelled
-from .node.tietu_node import FKTietuNode,FKShowBaseText
-
+from .node.tietu_node import FKTietuNode,FKShowBaseText,FK3DPOSENode
 
 NODE_CLASS_MAPPINGS = {
     "FK_Node": FKTietuNode,
     "FK_ShowBaseNode": FKShowBaseText,
+    "FK_3dpose": FK3DPOSENode,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "FK_Node": "FK_图片编码转换",
     "FK_ShowBaseNode": "FK_高级贴图编辑器",
+    "FK_3dpose": "FK_3D姿态编辑器(3D Openpose Editor)",
 }
 
 WEB_DIRECTORY = "web"
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
-VERSION = "1.2.9"
+VERSION = "1.3.0"
--- node/tietu_node.py
+++ node/tietu_node.py
@@ -1,7 +1,89 @@
 import base64
+import hashlib
+import os
 from io import BytesIO
 from PIL import Image
+import torch
 import numpy as np  
+import folder_paths
+
+class FK3DPOSENode(object):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(self):
+        temp_dir = folder_paths.get_temp_directory()
+        temp_dir = os.path.join(temp_dir, '3dposeeditor')
+        if not os.path.isdir(temp_dir):
+            os.makedirs(temp_dir)
+        return {
+            "required": {
+                "pose": ("STRING", ),
+                "depth": ("STRING", ),
+                "normal": ("STRING", ),
+                "canny": ("STRING", ),
+            },
+        } 
+
+    RETURN_TYPES = ("IMAGE","IMAGE","IMAGE","IMAGE","INT", "INT",)
+    RETURN_NAMES = ("OpenPose", "Depth", "Normal", "Canny","Width", "Height",)
+    FUNCTION = "get_pose"
+    CATEGORY = "FK_Nodes"
+
+    def get_pose(self, pose=None, depth=None, normal=None, canny=None):
+        if pose is None:
+            raise ValueError("请先进行姿态编辑")
+
+        temp_dir = folder_paths.get_temp_directory()
+        temp_dir = os.path.join(temp_dir, '3dposeeditor')
+        image_path = os.path.join(temp_dir, pose)
+        i = None
+        try:
+            i = Image.open(image_path)
+        except:
+            raise ValueError("请先进行姿态编辑")            
+        
+        poseImage = i.convert("RGB")
+        poseImage = np.array(poseImage).astype(np.float32) / 255.0
+        poseImage = torch.from_numpy(poseImage)[None,]
+
+        image_path = os.path.join(temp_dir, depth)
+
+        i = Image.open(image_path)
+        depthImage = i.convert("RGB")
+        depthImage = np.array(depthImage).astype(np.float32) / 255.0
+        depthImage = torch.from_numpy(depthImage)[None,]
+
+        image_path = os.path.join(temp_dir, normal)
+
+        i = Image.open(image_path)
+        normalImage = i.convert("RGB")
+        normalImage = np.array(normalImage).astype(np.float32) / 255.0
+        normalImage = torch.from_numpy(normalImage)[None,]
+
+        image_path = os.path.join(temp_dir, canny)
+
+        i = Image.open(image_path)
+        cannyImage = i.convert("RGB")
+        cannyImage = np.array(cannyImage).astype(np.float32) / 255.0
+        cannyImage = torch.from_numpy(cannyImage)[None,]
+        width, height = i.size
+        return (poseImage, depthImage, normalImage, cannyImage,width, height)
+
+    @staticmethod
+    def IS_CHANGED(self, pose=None, depth=None, normal=None, canny=None):
+        if pose is None:
+            return False
+
+        temp_dir = folder_paths.get_temp_directory()
+        temp_dir = os.path.join(temp_dir, '3dposeeditor')
+        image_path = os.path.join(temp_dir, pose)
+        m = hashlib.sha256()
+        with open(image_path, 'rb') as f:
+            m.update(f.read())
+        return m.digest().hex()
+  
 class FKTietuNode:
     INPUT_TYPES = {
          "required": {
@@ -42,7 +124,7 @@ class FKTietuNode:
                     except Exception as e:                    
                         base64_image3 = self.encode_image(Texture) or ""
             except Exception as e:
-                print(f"Texture处理错误: {str(e)}")        
+                raise ValueError(f"Texture处理错误: {str(e)}")        
         base64_image1 = self.encode_image(background) or ""
         base64_image2 = self.encode_image(Depth) or ""        
         return {"result": (base64_image1, base64_image2, base64_image3)}
